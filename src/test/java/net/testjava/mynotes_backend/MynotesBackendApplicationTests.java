0b0de83724d731e98d5e385eecbd53e78fae8f4d
fairytale4599 <158490055+fairytale4599@users.noreply.github.com>
2025-01-20 14:43:19 +0200

    First commit

---
+++ src/test/java/net/testjava/mynotes_backend/MynotesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.testjava.mynotes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynotesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
